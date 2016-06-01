c850020404b5bd0007089e04518501a61717c9a4
nithin <nithinjosetom@gmail.com>
2016-06-01 20:44:04 +0530

    Changed admin options

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from polls.models import Poll
+class PollAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question']
 
-admin.site.register(Poll)
+admin.site.register(Poll, PollAdmin)
