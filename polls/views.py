40d0582ba0306f0a7a57b61dc09509b6a6d34eb8
nithin <nithinjosetom@gmail.com>
2016-06-03 12:22:38 +0530

    Changed index view to show questions ordered by last published date

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # Create your views here.
+from polls.models import Poll
 def index(request):
-	return HttpResponse('You are at the poll index.')
+	latest_polls = Poll.objects.order_by('-pub_date')[:4]
+	output = ', '.join([p.question for p in latest_polls])
+	return HttpResponse(output)
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll %s." % poll_id)
 def results(request, poll_id):
