af00d13b2bcd8b7d8ba94cfe699b615b17fd9c23
nithin <nithinjosetom@gmail.com>
2016-06-04 19:20:32 +0530

    Debugged indexview

--- polls/views.py
+++ polls/views.py
@@ -7,8 +7,11 @@ class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_poll_list'
 	def get_queryset(self):
-		"""Return the last five published polls."""
-		return  Poll.objects.order_by('-pub_date')[:5]
+		"""
+		Return the last five published polls (not including those set to be
+		published in the future).
+		"""
+		return Poll.objects.filter(	pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 class DetailView(generic.DetailView):
 	model = Poll
 	template_name = 'polls/detail.html'
